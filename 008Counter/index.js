5f2a6524085699c2b67893d2b67f7456a120ab31
Chipp <87024769+chippest@users.noreply.github.com>
2024-03-05 13:53:54 +0500

    Counter Update
    now the counter CAN NOT BE COUNTERED!!

--- 008Counter/index.js
+++ 008Counter/index.js
@@ -5,22 +5,20 @@ let uinc = 1
 
 document.getElementById("usub").onclick = function(){
     uinc = Number(document.getElementById("uinc").value)
-    console.log(uinc)
 }
 
 let dinc = 1
 
 document.getElementById("dsub").onclick = function(){
     dinc = Number(document.getElementById("dinc").value)
-    console.log(dinc)
 }
 
 document.getElementById("up").onclick = function(){
-    count = count + uinc
+    count = count + Math.abs(uinc)
     document.getElementById("counter").textContent = `${count}`
 }
 document.getElementById("down").onclick = function(){
-    count = count - dinc
+    count = count - Math.abs(dinc)
     document.getElementById("counter").textContent = `${count}`
 }
 document.getElementById("reset").onclick = function(){
