b217706c6a55193dab3f741b30f3ad77030dca4b
Chipp <87024769+chippest@users.noreply.github.com>
2024-03-18 13:28:51 +0500

    Talking to Myself!
    LONLINESS CAN BE FUN TOO!!!

--- 011ChatUI/script.js
+++ 011ChatUI/script.js
@@ -5,33 +5,31 @@ let ConvoII
 
 document.getElementById("send1").onclick = function(){
     input = document.getElementById("inputmessage").value
-    console.log(input)
 
     prevConvo = document.getElementById("messages").innerHTML
-    console.log(prevConvo)
 
     ConvoI = `<div class="left"><span class="lefttext">${input}</span></div>`
-    console.log(ConvoI)
 
     if(document.getElementById("inputmessage").value != ''){
     document.getElementById("messages").innerHTML = prevConvo + ConvoI
     }
     
     document.getElementById("inputmessage").value = ''
+
+    document.getElementById("messages").scrollTop = document.getElementById("messages").scrollHeight;
 }
 document.getElementById("send2").onclick = function(){
     input = document.getElementById("inputmessage").value
-    console.log(input)
 
     prevConvo = document.getElementById("messages").innerHTML
-    console.log(prevConvo)
 
     ConvoI = `<div class="right"><span class="righttext">${input}</span></div>`
-    console.log(ConvoI)
     
     if(document.getElementById("inputmessage").value != ''){
     document.getElementById("messages").innerHTML = prevConvo + ConvoI
     }
     
     document.getElementById("inputmessage").value = ''
+
+    document.getElementById("messages").scrollTop = document.getElementById("messages").scrollHeight;
 }
