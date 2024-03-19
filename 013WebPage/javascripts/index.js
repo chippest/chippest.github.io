3792e5caaba2ca48adf4f89c0273818e9654c1fb
Chipp <87024769+chippest@users.noreply.github.com>
2024-03-19 14:13:53 +0500

    lunaria did it
    FR

--- 013WebPage/javascripts/index.js
+++ 013WebPage/javascripts/index.js
@@ -1,3 +1,7 @@
 document.getElementById("dotButton").addEventListener('click', function() {
     document.getElementById("hiddenMenuItems").classList.toggle('shown');
 });
+
+document.getElementById("Logbtn").onclick = function() {
+    window.location.href = "../html_files/login.html";
+};
--- 013WebPage/javascripts/login.js
+++ 013WebPage/javascripts/login.js
@@ -1,1 +1,23 @@
-alert("Hello,Hello!")
+document.getElementById("BtnLogin").onclick = function () {
+
+    const username = [];
+    const password = [];
+    username[0] = "ibtisam";
+    username[1] = "lulu";
+
+
+    password[0] = "123";
+    password[1] = "456";
+
+    for(i = 0; i <username.length; i++) {
+    for (j = 0; j < password.length; j++) {
+        if (document.getElementById("username").value == username[i] && document.getElementById("password").value == password[j]) {
+            window.location.href = "../html_files/index.html";
+            console.log("Hello world")
+        }
+        else {
+            alert("Error: inncorrect username or password");
+        }
+    }
+}
+};
