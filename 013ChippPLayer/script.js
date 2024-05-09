dde55f38da80eb995579f6908b2a6b0aa8d465c2
Chipp <87024769+chippest@users.noreply.github.com>
2024-05-09 10:58:18 +0500

    Music Player Functional
    added functionality to the seekbar, the  play, pause, as well as the mute button.
    also made it responsive for phones

--- 013ChippPLayer/script.js
+++ 013ChippPLayer/script.js
@@ -1,4 +1,28 @@
-function trigger(){
-    document.getElementById("playButton").classList.toggle("buttonOff")
-    document.getElementById("pauseButton").classList.toggle("buttonOff")
+let audioFile = document.getElementById("audioFile")
+let pButton = document.getElementById("playButton")
+let musicOn = false
+let mButton = document.getElementById("mutebutton")
+let volumeOn = true
+
+pButton.onclick = function(){
+    if(!musicOn){
+        audioFile.play()
+        pButton.innerHTML = "<i class='fa-solid fa-pause'></i>"
+        musicOn = true
+    } else if(musicOn){
+        audioFile.pause()
+        pButton.innerHTML = "<i class='fa-solid fa-play'></i>"
+        musicOn = false
+    }
+}
+mButton.onclick = function(){
+    if(volumeOn){
+        audioFile.muted = true
+        mButton.innerHTML = "<i class='fa-solid fa-volume-xmark'></i>"
+        volumeOn = false
+    } else if(!volumeOn){
+        audioFile.muted = false
+        mButton.innerHTML = "<i class='fa-solid fa-volume-high'>"
+        volumeOn = true
+    }
 }
