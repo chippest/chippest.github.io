afe357911d35f9a494b12622d92334873c89f366
Chipp <87024769+chippest@users.noreply.github.com>
2024-05-12 23:08:58 +0500

    music player plus!
    damn i put a lot into this thing

--- 013ChippPLayer/script.js
+++ 013ChippPLayer/script.js
@@ -4,38 +4,59 @@ let musicOn = false
 let mButton = document.getElementById("mutebutton")
 let volumeOn = true
 let rCont = document.getElementById(`rcont`)
-
+let root = document.querySelector(`:root`)
+let plbg = document.body
+let plicon = document.getElementById('playingIcon')
+let pltitle = document.getElementById('ptTitle')
+let plsgr = document.getElementById('ptSinger')
 
 function refresh() {
     location.reload()
 }
 
+// MUSIC FUNCTIONS
+function playSong(){
+    audioFile.play()
+    pButton.innerHTML = "<i class='fa-solid fa-pause'></i>"
+    musicOn = true
+}
+function pauseSong(){
+    audioFile.pause()
+    pButton.innerHTML = "<i class='fa-solid fa-play'></i>"
+    musicOn = false
+}
+function mute(){
+    audioFile.muted = true
+    mButton.innerHTML = "<i class='fa-solid fa-volume-xmark'></i>"
+    volumeOn = false
+}
+function unmute(){
+    audioFile.muted = false
+    mButton.innerHTML = "<i class='fa-solid fa-volume-high'>"
+    volumeOn = true
+}
+
+// MUSIC CONTROLLERS
 pButton.onclick = function(){
     if(!musicOn){
-        audioFile.play()
-        pButton.innerHTML = "<i class='fa-solid fa-pause'></i>"
-        musicOn = true
+        playSong()
     } else if(musicOn){
-        audioFile.pause()
-        pButton.innerHTML = "<i class='fa-solid fa-play'></i>"
-        musicOn = false
+        pauseSong()
     }
 }
 mButton.onclick = function(){
     if(volumeOn){
-        audioFile.muted = true
-        mButton.innerHTML = "<i class='fa-solid fa-volume-xmark'></i>"
-        volumeOn = false
+        mute()
     } else if(!volumeOn){
-        audioFile.muted = false
-        mButton.innerHTML = "<i class='fa-solid fa-volume-high'>"
-        volumeOn = true
+        unmute()
     }
 }
 
+
+// OPTION CONTAINERS
 function rightSlide(x){
-    let style = getComputedStyle(document.querySelector(`:root`))
-    let rightSize = style.getPropertyValue('--rightSize')
+    let root = getComputedStyle(document.querySelector(`:root`))
+    let rightSize = root.getPropertyValue('--rightSize')
     const finalSize = Number(rightSize.slice(0, -3))
 
     let abc = x * finalSize
@@ -43,15 +64,110 @@ function rightSlide(x){
     
     rCont.style.transform = `translateX(${translation}rem)`
 }
-rightSlide(2)
-
-
-
-
-function Song(avatar, name, singer, time, accent) {
-  this.avatar = avatar;
-  this.name = name;
-  this.singer = singer;
-  this.time = time;
-  this.accent = accent;
+    
+    
+    
+// ANDROID SECTIONS
+function sectionOne(){
+    document.getElementById("rightContainer").style.width = "0rem"
+    document.getElementById("buttons").style.width = "0rem"
+    setTimeout(() => {
+        document.getElementById("leftContainer").style.width = '18rem'
+        document.getElementById("rightContainer").style.width = "0rem"
+        document.getElementById("buttons").style.width = "0rem"
+    }, 700);
 }
+function sectionTwo(){
+    document.getElementById("leftContainer").style.width = "0rem"
+    setTimeout(() => {
+        document.getElementById("rightContainer").style.width = 'var(--rightSize)'
+        document.getElementById("buttons").style.width = '4rem'
+        document.getElementById("leftContainer").style.width = "0rem"
+    }, 700);
+}
+
+
+
+
+let sidHTML = document.querySelectorAll('.songID')
+let accentHTML = document.querySelectorAll('.accentSpan')
+let spicHTML = document.querySelectorAll('.songAvatar')
+let stitleHTML = document.querySelectorAll('.songTitle')
+let ssingerHTML = document.querySelectorAll('.songSinger')
+let suploaderHTML = document.querySelectorAll('.songUploader')
+
+
+let accentColors = []
+for (let i = 0; i < sidHTML.length; i++) {
+    let x = accentHTML[i].innerHTML
+    accentColors.push(x)
+}
+// console.log(accentColors)
+
+let avatarURLs = []
+for (let i = 0; i < sidHTML.length; i++) {
+    let x = sidHTML[i].innerHTML
+    avatarURLs.push(`icon${x}.jpg`)
+}
+// console.log(avatarURLs)
+
+let songTItles = []
+for (let i = 0; i < sidHTML.length; i++) {
+    let x = stitleHTML[i].innerHTML
+    songTItles.push(x)
+}
+// console.log(songTItles)
+
+let singerNames = []
+for (let i = 0; i < sidHTML.length; i++) {
+    let x = ssingerHTML[i].innerHTML
+    singerNames.push(x)
+}
+// console.log(singerNames)
+
+let songURLs = []
+for (let i = 0; i < sidHTML.length; i++) {
+    let x = sidHTML[i].innerHTML
+    songURLs.push(`song${x}.mp3`)
+}
+// console.log(songURLs)
+
+let songItem = document.querySelectorAll('.songItem')
+    
+function changePlayerInfo(x){
+    root.style.setProperty('--iconAccent', x.accent)
+    plbg.style.backdropFilter = `blur(3rem) brightness(0%) saturate(20%)`
+    plicon.style.filter = `brightness(0%)`
+    setTimeout(() => {
+        plbg.style.backgroundImage = `url(${x.avatar}`
+        plicon.src = x.avatar
+        plbg.style.backdropFilter = `blur(3rem) brightness(50%) saturate(20%)`
+        plicon.style.filter = `brightness(100%)`
+    }, 500);
+    pltitle.innerText = x.title
+    plsgr.innerText = x.singer
+    audioFile.src = x.url
+    playSong()
+    if (window.innerWidth <= 520) {
+        sectionOne()
+    }
+}
+
+function Song(accent, id, avatar, title, singer, url) {
+    this.accent = accent;
+    this.id = id;
+    this.avatar = avatar;
+    this.title = title;
+    this.singer = singer;
+    this.url = url;
+}
+
+let songList = []
+
+for (let i = 0; i < songItem.length; i++) {
+    songList[i] = new Song(accentColors[i], sidHTML[i].innerHTML, avatarURLs[i], songTItles[i], singerNames[i], songURLs[i])
+}
+
+
+
+
