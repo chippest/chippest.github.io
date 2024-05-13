751af074c8a0f77cf67d62a2efbd71bc982c29cd
Chipp <87024769+chippest@users.noreply.github.com>
2024-05-13 15:38:09 +0500

    Music Player Functional
    the music player is now functional

--- 013ChippPLayer/script.js
+++ 013ChippPLayer/script.js
@@ -9,10 +9,53 @@ let plbg = document.body
 let plicon = document.getElementById('playingIcon')
 let pltitle = document.getElementById('ptTitle')
 let plsgr = document.getElementById('ptSinger')
+// SONG LIBRARY
+let songID = ['1', '2', '3', '4', '5', '6', '7']
+let songFull = ['Thousand Miles', 'Talk To You', 'Hearts', 'Paper Planes', 'Atlantis', 'Sway To My Beat In Cosmos', 'Divine']
+let songName = []
+for (let i = 0; i < songID.length; i++) {
+    let first = songFull[i]
+    let second
+    if(songFull[i].length > 13){
+    second = first.slice(0, 10) + `...`
+    songName.push(second)
+    } else{
+        songName.push(first)
+    }
+}
+let singerName = [`the Kid LAROI`, 'Zach B', 'Zach B', 'M.I.A.', 'Seafret', 'Robin', 'Krishnahazar']
+let uploader = [`chipp`, 'chipp', 'chipp', 'Maxi', 'Monkey', 'Mahdi', 'Hell']
+let accent = [`#978e75`, `#c264a6`, '#272571', '#715755', '#564E4B', '#846699', '#AD825D']
 
+let jpglink = `https://lets.linkpc.net/icons/`
+let avatar = []
+for (let i = 0; i < songID.length; i++) {
+    avatar.push(`${jpglink}${songID[i]}.jpg`)
+}
+let mp3link = `https://lets.linkpc.net/songs/`
+let audio = []
+for (let i = 0; i < songID.length; i++) {
+    audio.push(`${mp3link}${songID[i]}.mp3`)
+}
+let songItem = []
 function refresh() {
     location.reload()
 }
+let songList = []
+for (let i = 0; i < songID.length; i++) {
+    songList[i] = new Song(songID[i], songFull[i], songName[i], singerName[i], uploader[i], accent[i], avatar[i], audio[i])
+}
+let songCont = document.getElementById('songList')
+
+
+
+
+
+
+
+
+
+
 
 // MUSIC FUNCTIONS
 function playSong(){
@@ -36,6 +79,52 @@ function unmute(){
     volumeOn = true
 }
 
+let shuffled = false
+
+let songNumber = 0
+function next(){
+    if(!shuffled){
+    songNumber++
+    if (songNumber >= songID.length){
+        songNumber = 0
+    }
+    changePlayerInfo(songList[songNumber])}
+    else if(shuffled){
+        shuffle()
+        changePlayerInfo(songList[songNumber])
+    }
+}
+function prev(){
+    if(!shuffled){
+        songNumber--
+    if (songNumber == 0){
+        songNumber = songID.length
+    }
+    changePlayerInfo(songList[songNumber])}
+    else if(shuffled){
+        shuffle()
+        changePlayerInfo(songList[songNumber])
+    }
+}
+
+function shuffle(){
+    let x
+    let y
+    x = Math.floor(Math.random() * songID.length) + 0
+    y = x
+    if (x == songNumber){
+        y = Math.floor(Math.random() * songID.length) + 0
+    }
+    songNumber = y
+    console.log(songNumber)
+}
+
+
+
+
+
+let sButton = document.getElementById('shufButton')
+
 // MUSIC CONTROLLERS
 pButton.onclick = function(){
     if(!musicOn){
@@ -47,10 +136,22 @@ pButton.onclick = function(){
 mButton.onclick = function(){
     if(volumeOn){
         mute()
+        mButton.style.color = 'var(--iconAccent)'
     } else if(!volumeOn){
         unmute()
+        mButton.style.color = 'white'
     }
 }
+sButton.onclick = function(){
+    if(shuffled){
+        shuffled = false
+        sButton.style.color = 'white'
+    } else if(!shuffled){
+        shuffled = true
+        sButton.style.color = 'var(--iconAccent)'
+    }
+}
+
 
 
 // OPTION CONTAINERS
@@ -64,6 +165,7 @@ function rightSlide(x){
     
     rCont.style.transform = `translateX(${translation}rem)`
 }
+rightSlide(1)
     
     
     
@@ -88,52 +190,37 @@ function sectionTwo(){
 
 
 
-
-let sidHTML = document.querySelectorAll('.songID')
-let accentHTML = document.querySelectorAll('.accentSpan')
-let spicHTML = document.querySelectorAll('.songAvatar')
-let stitleHTML = document.querySelectorAll('.songTitle')
-let ssingerHTML = document.querySelectorAll('.songSinger')
-let suploaderHTML = document.querySelectorAll('.songUploader')
-
-
-let accentColors = []
-for (let i = 0; i < sidHTML.length; i++) {
-    let x = accentHTML[i].innerHTML
-    accentColors.push(x)
+// MUSIC
+function Song(id, fullname, name, singer, uploader, accent, avatar, audio) {
+    this.id = id;
+    this.fullname = fullname
+    this.name = name;
+    this.singer = singer;
+    this.uploader = uploader;
+    this.accent = accent;
+    this.avatar = avatar;
+    this.audio = audio;
 }
-// console.log(accentColors)
-
-let avatarURLs = []
-for (let i = 0; i < sidHTML.length; i++) {
-    let x = sidHTML[i].innerHTML
-    avatarURLs.push(`icon${x}.jpg`)
+for (let i = 0; i < songID.length; i++) {
+    songCont.innerHTML += `<div id="song${songID[i]}" class="row songItem" style="--iconAccent: ${accent[i]}50;" onclick="changePlayerInfo(songList[${i}])"></div>`
+    songItem[i] = document.getElementById(`song${songID[i]}`)
+    songItem[i].innerHTML = `
+    <img src="${songList[i].avatar}" alt="" class="songAvatar">
+    <div class="songText column">
+        <div class="songUp row">
+            <span class="songTitle">${songList[i].fullname}</span>
+            <span class="songID">${songList[i].id}</span>
+        </div>
+        <div class="songUp row">
+            <span class="songSinger">${songList[i].singer}</span>
+            <span class="songUploader">${songList[i].uploader}</span>
+        </div>
+    </div>
+    `
+    console.log(songItem[i])
 }
-// console.log(avatarURLs)
-
-let songTItles = []
-for (let i = 0; i < sidHTML.length; i++) {
-    let x = stitleHTML[i].innerHTML
-    songTItles.push(x)
-}
-// console.log(songTItles)
-
-let singerNames = []
-for (let i = 0; i < sidHTML.length; i++) {
-    let x = ssingerHTML[i].innerHTML
-    singerNames.push(x)
-}
-// console.log(singerNames)
-
-let songURLs = []
-for (let i = 0; i < sidHTML.length; i++) {
-    let x = sidHTML[i].innerHTML
-    songURLs.push(`song${x}.mp3`)
-}
-// console.log(songURLs)
-
-let songItem = document.querySelectorAll('.songItem')
-    
+document.getElementById(`song1`).classList.add('songNow')
+document.getElementById('ptTitle').innerText = songList[0].name
 function changePlayerInfo(x){
     root.style.setProperty('--iconAccent', x.accent)
     plbg.style.backdropFilter = `blur(3rem) brightness(0%) saturate(20%)`
@@ -144,30 +231,16 @@ function changePlayerInfo(x){
         plbg.style.backdropFilter = `blur(3rem) brightness(50%) saturate(20%)`
         plicon.style.filter = `brightness(100%)`
     }, 500);
-    pltitle.innerText = x.title
+    pltitle.innerText = x.name
     plsgr.innerText = x.singer
-    audioFile.src = x.url
+    audioFile.src = x.audio
     playSong()
     if (window.innerWidth <= 520) {
         sectionOne()
     }
+    songNumber = x.id - 1
+    for (let i = 0; i < songID.length; i++) {
+        document.getElementById(`song${songID[i]}`).classList.remove('songNow')
+    }
+    document.getElementById(`song${x.id}`).classList.add('songNow')
 }
-
-function Song(accent, id, avatar, title, singer, url) {
-    this.accent = accent;
-    this.id = id;
-    this.avatar = avatar;
-    this.title = title;
-    this.singer = singer;
-    this.url = url;
-}
-
-let songList = []
-
-for (let i = 0; i < songItem.length; i++) {
-    songList[i] = new Song(accentColors[i], sidHTML[i].innerHTML, avatarURLs[i], songTItles[i], singerNames[i], songURLs[i])
-}
-
-
-
-
