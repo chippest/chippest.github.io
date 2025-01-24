b08fcb53a5fd37f34427d0a0e82f98cafa73a46d
Chipp <ibtisamaleem6@gmail.com>
2025-01-24 19:57:48 +0500

    removed router functionality temporarily

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import {
-  Link,
-  Navigate,
-  Route,
-  Routes,
-  useLocation,
-  useNavigate,
-} from "react-router-dom";
 import { Page } from "./Classes/Pages";
 import { Home } from "./Pages/Home";
 import { About } from "./Pages/About";
 import { Contact } from "./Pages/Contact";
 import { Projects } from "./Pages/Projects";
+import { render } from "react-dom";
 
 function App() {
   const pages = [
@@ -23,78 +16,29 @@ function App() {
   ];
   let pageTitles = pages.map((page) => page.title.toLowerCase());
   const [currentPage, setCurrentPage] = useState(null);
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const path = location.pathname.substring(1);
-    setCurrentPage(path);
-  }, [location]);
-
-  useEffect(() => {
-    if (currentPage === null) {
-      navigate("/");
-    }
-  }, [currentPage, navigate]);
-
-  useEffect(() => {
-    if (currentPage) {
-      const timer = setTimeout(() => {
-        const pageElement = document.querySelector(`.page.${currentPage}`);
-        if (pageElement) {
-          pageElement.classList.add("open");
-        }
-      }, 50); // Delay in milliseconds
-      return () => clearTimeout(timer);
-    }
-  }, [currentPage]);
+  const [lastPage, setLastPage] = useState("home");
 
   return (
     <>
       <div className="app">
         <div className="pages">
-          {pages.map((page, index) => (
-            <>
-              <Link to={`/${page.title.toLowerCase()}`}>
+          {pages.map((page, index) => {
+            const rendered = false;
+            const pageL = page.title.toLowerCase();
+            return (
+              <>
                 <button
                   onClick={() => {
-                    currentPage === page.title.toLowerCase()
-                      ? setCurrentPage(null)
-                      : setCurrentPage(page.title.toLowerCase());
+                    if (currentPage === pageL) return;
+                    setLastPage(currentPage);
+                    setCurrentPage(pageL);
                   }}
                 >
                   {page.title}
                 </button>
-              </Link>
-              <Routes>
-                <Route
-                  path={`/${page.title.toLowerCase()}`}
-                  element={
-                    <div className={`page ${page.title.toLowerCase()}`}>
-                      {page.title.toLowerCase() === currentPage &&
-                      currentPage === "home" ? (
-                        <Home />
-                      ) : currentPage === "about" ? (
-                        <About />
-                      ) : currentPage === "contact" ? (
-                        <Contact />
-                      ) : (
-                        currentPage === "projects" && <Projects />
-                      )}
-                    </div>
-                  }
-                />
-                <Route
-                  path="*"
-                  element={
-                    pageTitles.includes(location.pathname.slice(1)) ? null : (
-                      <Navigate to="/" />
-                    )
-                  }
-                />
-              </Routes>
-            </>
-          ))}
+              </>
+            );
+          })}
         </div>
       </div>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </StrictMode>
 );
