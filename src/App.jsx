96562784ae3626d5cfc67c425f74e0348933c4ec
Chipp <ibtisamaleem6@gmail.com>
2025-01-25 23:13:35 +0500

    added back HashRouter functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 import { useState, useEffect } from "react";
+import {
+  Route,
+  Routes,
+  Navigate,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import "./App.css";
 import { Page } from "./Classes/Pages";
 import { Home } from "./Pages/Home";
@@ -16,11 +23,30 @@ function App() {
   const [currentPage, setCurrentPage] = useState(null);
   const [renderedPage, setRenderedPage] = useState(null);
   const [fullButtons, setFullButtons] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const path = location.pathname.substring(1);
+    if (pages.map((page) => page.title.toLowerCase()).includes(path)) {
+      setCurrentPage(path);
+      setRenderedPage(path);
+      setTimeout(() => {
+        const pageElement = document.querySelector(`.page.${path}`);
+        if (pageElement) {
+          pageElement.classList.add("open");
+        }
+      }, 500); // Delay before adding the "open" class
+    } else {
+      setCurrentPage(null);
+      setRenderedPage(null);
+    }
+  }, [location]);
 
   const handlePageChange = (pageTitle) => {
     const newPage = pageTitle.toLowerCase();
     if (currentPage === newPage) {
-      // If the current page is already rendered, unrender it
+      // If the current page is already rendered, unrender it and navigate to default URL
       const pageElement = document.querySelector(`.page.${newPage}`);
       if (pageElement) {
         pageElement.classList.remove("open");
@@ -28,86 +54,87 @@ function App() {
           setRenderedPage(null);
           setCurrentPage(null);
           setFullButtons(false);
+          navigate("/");
         }, 500); // Delay to match the CSS transition
       }
     } else {
-      let t;
-      if (fullButtons) {
-        t = 0;
-      } else {
-        t = 500;
-      }
       setFullButtons(true);
-      // If a different page is rendered, unrender the previous page
-      setTimeout(() => {
-        if (currentPage) {
-          const previousPageElement = document.querySelector(
-            `.page.${currentPage}`
-          );
-          if (previousPageElement) {
-            previousPageElement.classList.remove("open");
-            setTimeout(() => {
-              setRenderedPage(null);
-              setCurrentPage(newPage);
-              setRenderedPage(newPage);
-              setTimeout(() => {
-                const newPageElement = document.querySelector(
-                  `.page.${newPage}`
-                );
-                if (newPageElement) {
-                  newPageElement.classList.add("open");
-                }
-              }, 50); // Delay before adding the "open" class
-            }, 500); // Delay to match the CSS transition
-          }
-        } else {
-          // If no page is currently rendered, render the new page
-          setCurrentPage(newPage);
-          setRenderedPage(newPage);
+      if (currentPage) {
+        // If a different page is rendered, unrender the previous page
+        const previousPageElement = document.querySelector(
+          `.page.${currentPage}`
+        );
+        if (previousPageElement) {
+          previousPageElement.classList.remove("open");
           setTimeout(() => {
-            const newPageElement = document.querySelector(`.page.${newPage}`);
-            if (newPageElement) {
-              newPageElement.classList.add("open");
-            }
-          }, 100); // Delay before adding the "open" class
+            setRenderedPage(null);
+            setCurrentPage(newPage);
+            setRenderedPage(newPage);
+            navigate(`/${newPage}`);
+            setTimeout(() => {
+              const newPageElement = document.querySelector(`.page.${newPage}`);
+              if (newPageElement) {
+                newPageElement.classList.add("open");
+              }
+            }, 100); // Delay before adding the "open" class
+          }, 500); // Delay to match the CSS transition
         }
-      }, t);
+      } else {
+        // If no page is currently rendered, render the new page
+        setCurrentPage(newPage);
+        setRenderedPage(newPage);
+        navigate(`/${newPage}`);
+        setTimeout(() => {
+          const newPageElement = document.querySelector(`.page.${newPage}`);
+          if (newPageElement) {
+            newPageElement.classList.add("open");
+          }
+        }, 100); // Delay before adding the "open" class
+      }
     }
   };
 
   return (
-    <>
-      <div className="app">
-        <div className={`pages ${fullButtons && "gapped"}`}>
-          {pages.map((page, index) => {
-            const title = page.title.toLowerCase();
-            return (
-              <>
-                <button
-                  onClick={() => handlePageChange(page.title)}
-                  className={fullButtons && "full"}
-                >
-                  <span>{page.title}</span>
-                </button>
-                {renderedPage === title && (
-                  <div className={`page ${title}`}>
-                    {title === "home" ? (
-                      <Home />
-                    ) : title === "about" ? (
-                      <About />
-                    ) : title === "contact" ? (
-                      <Contact />
-                    ) : (
-                      title === "projects" && <Projects />
-                    )}
-                  </div>
-                )}
-              </>
-            );
-          })}
-        </div>
+    <div className="app">
+      <div className={`pages ${fullButtons ? "gapped" : ""}`}>
+        {pages.map((page, index) => {
+          const title = page.title.toLowerCase();
+          return (
+            <>
+              <button
+                onClick={() => handlePageChange(page.title)}
+                className={fullButtons && "full"}
+              >
+                <span>{page.title}</span>
+              </button>
+              {renderedPage === title && (
+                <div className={`page ${title}`}>
+                  {title === "home" ? (
+                    <Home />
+                  ) : title === "about" ? (
+                    <About />
+                  ) : title === "contact" ? (
+                    <Contact />
+                  ) : (
+                    title === "projects" && <Projects />
+                  )}
+                </div>
+              )}
+            </>
+          );
+        })}
       </div>
-    </>
+      <Routes>
+        {pages.map((page, index) => (
+          <Route
+            key={index}
+            path={`/${page.title.toLowerCase()}`}
+            element={<div />}
+          />
+        ))}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </div>
   );
 }
 
