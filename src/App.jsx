68efd41c6aadb8d5c90ffb4f1fea250d1a0fc7d1
Chipp <ibtisamaleem6@gmail.com>
2025-02-14 06:48:34 -0800

    added router compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { About } from "./pages/About";
-import { Contact } from "./pages/Contact";
+import { Route, Routes, Link } from "react-router-dom";
 import { Projects } from "./Pages/Projects";
+import { Contact } from "./pages/Contact";
+import { About } from "./pages/About";
 
 function App() {
   const pages = [
@@ -14,11 +15,25 @@ function App() {
       <h1>Chipp</h1>
       <div className="container">
         <div className="pageNav">
-          <div className="button">About</div>
-          <div className="button">Projects</div>
-          <div className="button">Contact</div>
+          <div className="button">
+            <Link to="/about">About</Link>
+          </div>
+          <div className="button">
+            <Link to="/projects">Projects</Link>
+          </div>
+          <div className="button">
+            <Link to="/contact">Contact</Link>
+          </div>
         </div>
-        <div className="page"></div>
+        <div className="page">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+            {/* Optionally add a fallback route */}
+            {/* <Route path="*" element={<About />} /> */}
+          </Routes>
+        </div>
       </div>
     </div>
   );
