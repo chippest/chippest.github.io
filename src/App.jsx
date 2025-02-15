1c0c7c43c06290cae1204a684a4f1871283002b5
Chipp <ibtisamaleem6@gmail.com>
2025-02-15 05:55:12 -0800

    added color variables on screen for references

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
+import "./app.css";
 import { Projects } from "./Pages/Projects";
 import { Contact } from "./pages/Contact";
 import { About } from "./pages/About";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("About");
+  const colorVariables = [
+    { name: "--c1", value: "#092635" },
+    { name: "--c2", value: "#1b4242" },
+    { name: "--c3", value: "#5c8374" },
+    { name: "--c4", value: "#9ec8b9" },
+  ];
 
   return (
     <div className="app">
@@ -26,6 +33,17 @@ function App() {
           {selectedPage === "Projects" && <Projects />}
           {selectedPage === "Contact" && <Contact />}
         </div>
+      </div>
+      <div className="colorRef">
+        {colorVariables.map((color) => (
+          <div
+            key={color.name}
+            className="colorItem"
+            style={{ backgroundColor: color.value }}
+          >
+            <span>{color.name}</span>
+          </div>
+        ))}
       </div>
     </div>
   );
