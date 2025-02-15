a11e396a556d7760a00df3c6f8ce7f980a77e799
Chipp <ibtisamaleem6@gmail.com>
2025-02-15 05:47:23 -0800

    added simple button navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
+import React, { useState } from "react";
 import { Projects } from "./Pages/Projects";
 import { Contact } from "./pages/Contact";
 import { About } from "./pages/About";
 
 function App() {
-  const pages = [
-    { name: "About" },
-    { name: "Projects" },
-    { name: "Contact" },
-    { name: "" },
-  ];
+  const [selectedPage, setSelectedPage] = useState("About");
+
   return (
     <div className="app">
       <h1>Chipp</h1>
       <div className="container">
         <div className="pageNav">
-          <div className="button">About</div>
-          <div className="button">Projects</div>
-          <div className="button">Contact</div>
+          <div className="button" onClick={() => setSelectedPage("About")}>
+            About
+          </div>
+          <div className="button" onClick={() => setSelectedPage("Projects")}>
+            Projects
+          </div>
+          <div className="button" onClick={() => setSelectedPage("Contact")}>
+            Contact
+          </div>
         </div>
         <div className="page">
-          <About />
-          <Projects />
-          <Contact />
+          {selectedPage === "About" && <About />}
+          {selectedPage === "Projects" && <Projects />}
+          {selectedPage === "Contact" && <Contact />}
         </div>
       </div>
     </div>
